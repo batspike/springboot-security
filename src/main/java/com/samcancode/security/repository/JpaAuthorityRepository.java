19e9c7a57bcd988617c971f47d48faa9fd1335d8
Sam Lau <samlau.sam@gmail.com>
2020-10-06 17:59:18 +0800

    setup for Resource Server

--- src/main/java/com/samcancode/security/repository/JpaAuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.samcancode.security.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.samcancode.security.domain.JpaAuthority;
-
-public interface JpaAuthorityRepository extends JpaRepository<JpaAuthority, Integer> {
-
-}
